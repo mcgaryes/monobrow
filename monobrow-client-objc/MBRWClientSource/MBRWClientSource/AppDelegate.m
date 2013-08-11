72f0f1ee5b6a9c9ac2a60569caa1d010188e9858
Eric McGary <emcgary@resource.com>
2013-08-11 13:27:25 -0400

    Adding Objective c files back in after gitignore

---
+++ monobrow-client-objc/MBRWClientSource/MBRWClientSource/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MBRWClientSource
+//
+//  Created by Eric McGary on 8/11/13.
+//  Copyright (c) 2013 Eric McGary. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
